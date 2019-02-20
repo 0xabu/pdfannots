9500a18ded203cd2f77c7ced425187a3008aade3
Andrew Baumann <Andrew.Baumann@microsoft.com>
2019-02-20 13:11:25 -0800

    refactoring and improvements
     * refactor to make things a bit more OOish; introduce Page, Pos classes
     * sort annotations, rather than using the order they appear in the file
     * improve output of multi-line annotations (don't join them all together)
     * make annotation text codec configurable, default to cp1252 for me :)

--- pdfannots.py
+++ pdfannots.py
@@ -5,8 +5,7 @@
 Extracts annotations from a PDF file in a text format for use in reviewing.
 """
 
-from __future__ import print_function
-import sys, io, textwrap, argparse
+import sys, io, textwrap, argparse, codecs
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.layout import LAParams, LTContainer, LTAnno, LTText, LTTextBox
@@ -91,9 +90,21 @@ class RectExtractor(TextConverter):
 
         render(ltpage)
 
-class Annotation:
-    def __init__(self, pageno, tagname, coords=None, rect=None, contents=None):
+class Page:
+    def __init__(self, pageno, mediabox):
         self.pageno = pageno
+        self.mediabox = mediabox
+        self.annots = []
+
+    def __eq__(self, other):
+        return self.pageno == other.pageno
+
+    def __lt__(self, other):
+        return self.pageno < other.pageno
+
+class Annotation:
+    def __init__(self, page, tagname, coords=None, rect=None, contents=None):
+        self.page = page
         self.tagname = tagname
         if contents == '':
             self.contents = None
@@ -139,9 +150,49 @@ class Annotation:
             (x0, y0, x1, y1) = self.boxes[0]
         else:
             return None
-        return (min(x0, x1), max(y0, y1)) # assume left-to-right top-to-bottom text :)
+        # XXX: assume left-to-right top-to-bottom text
+        return Pos(self.page, min(x0, x1), max(y0, y1))
 
-def getannots(pdfannots, pageno):
+    # custom < operator for sorting
+    def __lt__(self, other):
+        return self.getstartpos() < other.getstartpos()
+
+class Pos:
+    def __init__(self, page, x, y):
+        self.page = page
+        self.x = x
+        self.y = y
+
+    def __lt__(self, other):
+        if self.page < other.page:
+            return True
+        elif self.page == other.page:
+            assert self.page is other.page
+            # XXX: assume two-column left-to-right top-to-bottom documents
+            (sx, sy) = self.normalise_to_mediabox()
+            (ox, oy) = other.normalise_to_mediabox()
+            (x0, y0, x1, y1) = self.page.mediabox
+            colwidth = (x1 - x0) / 2
+            self_col = (sx - x0) // colwidth
+            other_col = (ox - x0) // colwidth
+            return self_col < other_col or (self_col == other_col and sy > oy)
+        else:
+            return False
+
+    def normalise_to_mediabox(self):
+        x, y = self.x, self.y
+        (x0, y0, x1, y1) = self.page.mediabox
+        if x < x0:
+            x = x0
+        elif x > x1:
+            x = x1
+        if y < y0:
+            y = y0
+        elif y > y1:
+            y = y1
+        return (x, y)
+
+def getannots(pdfannots, page, codec):
     annots = []
     for pa in pdfannots:
         subtype = pa.get('Subtype')
@@ -150,65 +201,35 @@ def getannots(pdfannots, pageno):
 
         contents = pa.get('Contents')
         if contents is not None:
-            contents = str(contents, 'iso8859-15') #'utf-8'
+            contents, _ = codec.decode(contents)
             contents = contents.replace('\r\n', '\n').replace('\r', '\n')
 
         coords = pdftypes.resolve1(pa.get('QuadPoints'))
         rect = pdftypes.resolve1(pa.get('Rect'))
-        a = Annotation(pageno, subtype.name.lower(), coords, rect, contents)
+        a = Annotation(page, subtype.name.lower(), coords, rect, contents)
         annots.append(a)
 
     return annots
 
-def normalise_to_box(pos, box):
-    (x, y) = pos
-    (x0, y0, x1, y1) = box
-    if x < x0:
-        x = x0
-    elif x > x1:
-        x = x1
-    if y < y0:
-        y = y0
-    elif y > y1:
-        y = y1
-    return (x, y)
-
-def nearest_outline(outlines, pageno, mediabox, pos):
-    (x, y) = normalise_to_box(pos, mediabox)
-    prev = None
-    for o in outlines:
-        if o.pageno < pageno:
-            prev = o
-        elif o.pageno > pageno:
-            return prev
-        else:
-            # XXX: assume two-column left-to-right top-to-bottom documents
-            (ox, oy) = normalise_to_box((o.x, o.y), mediabox)
-            (x0, y0, x1, y1) = mediabox
-            colwidth = (x1 - x0) / 2
-            outline_col = (ox - x0) // colwidth
-            pos_col = (x - x0) // colwidth
-            if outline_col > pos_col or (outline_col == pos_col and o.y < y):
-                return prev
-            else:
-                prev = o
-    return prev
-
-
 def prettyprint(packed_annots, outfile, wrap, sections):
+    (annots, outlines) = packed_annots
 
-    (annots, outlines, mediaboxes) = packed_annots
+    def nearest_outline(pos):
+        prev = None
+        for o in outlines:
+            if o.pos < pos:
+                prev = o
+            else:
+                break
+        return prev
 
     def fmtpos(annot):
         apos = annot.getstartpos()
-        if apos:
-            o = nearest_outline(outlines, annot.pageno, mediaboxes[annot.pageno], apos)
-        else:
-            o = None
+        o = nearest_outline(apos) if apos else None
         if o:
-            return "Page %d (%s):" % (annot.pageno + 1, o.title)
+            return "Page %d (%s): " % (annot.page.pageno + 1, o.title)
         else:
-            return "Page %d:" % (annot.pageno + 1)
+            return "Page %d: " % (annot.page.pageno + 1)
 
     def fmttext(annot):
         if annot.boxes:
@@ -220,22 +241,40 @@ def prettyprint(packed_annots, outfile, wrap, sections):
             return ''
 
     if wrap:
-        tw = textwrap.TextWrapper(width=wrap, initial_indent=" * ", subsequent_indent="   ")
+        # we'll need two text wrappers, one for the leading bullet on the first paragraph, one without the bullet.
+        tw1 = textwrap.TextWrapper(width=wrap, initial_indent=" * ", subsequent_indent="   ")
+        tw2 = textwrap.TextWrapper(width=wrap, initial_indent="   ", subsequent_indent="   ")
 
-    def printitem(*args):
-        msg = ' '.join(args)
+    def printitem(annot, part1, part2=None):
+        # break each part into paragraphs
+        lines1 = [l for l in part1.splitlines() if l]
+        assert lines1
+        lines2 = [l for l in part2.splitlines() if l] if part2 else None
+
+        if lines2:
+            # If we have a short text section and a short comment, join them
+            # into one paragraph. Otherwise, we'll use multiple output paragraphs.
+            if len(lines1) == 1 and len(lines2) == 1:
+                msglines = [lines1[0] + " -- " + lines2[0]]
+            else:
+                msglines = lines1 + ["-- " + lines2[0]] + lines2[1:]
+        else:
+            msglines = lines1
+
+        # prepend the formatted position
+        msglines[0] = fmtpos(annot) + msglines[0]
 
         if wrap:
-            msg = tw.fill(msg)
+            msg = tw1.fill(msglines[0]) + ('\n\n' if msglines[1:] else '') + '\n\n'.join(tw2.fill(m) for m in msglines[1:])
         else:
-            msg = " * " + msg
+            msg = " * " + msglines[0] + ('\n' if msglines[1:] else '') + '\n'.join('   ' + m for m in msglines[1:])
 
         print(msg + "\n", file=outfile)
 
     def printheader(name):
         # emit blank separator line if needed
         if printheader.called:
-            print("\n", file=outfile)
+            print("", file=outfile)
         else:
             printheader.called = True
         print("## " + name + "\n", file=outfile)
@@ -249,25 +288,25 @@ def prettyprint(packed_annots, outfile, wrap, sections):
         if highlights and secname == 'highlights':
             printheader("Highlights")
             for a in highlights:
-                printitem(fmtpos(a), fmttext(a))
+                printitem(a, fmttext(a))
 
         if comments and secname == 'comments':
             printheader("Detailed comments")
             for a in comments:
                 text = fmttext(a)
                 if text:
-                    printitem(fmtpos(a), text, "--", a.contents)
+                    printitem(a, text, a.contents)
                 else:
-                    printitem(fmtpos(a), a.contents)
+                    printitem(a, a.contents)
 
         if nits and secname == 'nits':
             printheader("Nits")
             for a in nits:
                 text = fmttext(a)
                 if a.contents:
-                    printitem(fmtpos(a), text, "--", a.contents)
+                    printitem(a, text, a.contents)
                 else:
-                    printitem(fmtpos(a), "%s" % text)
+                    printitem(a, text)
 
 def resolve_dest(doc, dest):
     if isinstance(dest, bytes):
@@ -279,12 +318,10 @@ def resolve_dest(doc, dest):
     return dest
 
 class Outline:
-    def __init__(self, title, dest, pageno, x, y):
+    def __init__(self, title, dest, pos):
         self.title = title
         self.dest = dest
-        self.pageno = pageno
-        self.x = x
-        self.y = y
+        self.pos = pos
 
 def get_outlines(doc, pagesdict):
     result = []
@@ -298,12 +335,13 @@ def get_outlines(doc, pagesdict):
         if destname is None:
             continue
         dest = resolve_dest(doc, destname)
-        pageno = pagesdict[dest[0].objid]
+        page = pagesdict[dest[0].objid]
         (_, _, targetx, targety, _) = dest
-        result.append(Outline(title, destname, pageno, targetx, targety))
+        pos = Pos(page, targetx, targety)
+        result.append(Outline(title, destname, pos))
     return result
 
-def process_file(fh, emit_progress):
+def process_file(fh, codec, emit_progress):
     rsrcmgr = PDFResourceManager()
     laparams = LAParams()
     device = RectExtractor(rsrcmgr, laparams=laparams)
@@ -311,32 +349,30 @@ def process_file(fh, emit_progress):
     parser = PDFParser(fh)
     doc = PDFDocument(parser)
 
-    pagesdict = {}
-    mediaboxes = {}
+    pagesdict = {} # map from PDF page object ID to Page object
     allannots = []
 
-    for (pageno, page) in enumerate(PDFPage.create_pages(doc)):
-        pagesdict[page.pageid] = pageno
-        mediaboxes[pageno] = page.mediabox
-        if page.annots is None or page.annots == []:
-            continue
+    for (pageno, pdfpage) in enumerate(PDFPage.create_pages(doc)):
+        page = Page(pageno, pdfpage.mediabox)
+        pagesdict[pdfpage.pageid] = page
+        if pdfpage.annots:
+            # emit progress indicator
+            if emit_progress:
+                sys.stderr.write((" " if pageno > 0 else "") + "%d" % (pageno + 1))
+                sys.stderr.flush()
 
-        # emit progress indicator
-        if emit_progress:
-            sys.stderr.write((" " if pageno > 0 else "") + "%d" % (pageno + 1))
-            sys.stderr.flush()
+            pdfannots = []
+            for a in pdftypes.resolve1(pdfpage.annots):
+                if isinstance(a, pdftypes.PDFObjRef):
+                    pdfannots.append(a.resolve())
+                else:
+                    sys.stderr.write('Warning: unknown annotation: %s\n' % a)
 
-        pdfannots = []
-        for a in pdftypes.resolve1(page.annots):
-            if isinstance(a, pdftypes.PDFObjRef):
-                pdfannots.append(a.resolve())
-            else:
-                sys.stderr.write('Warning: unknown annotation: %s\n' % a)
-
-        pageannots = getannots(pdfannots, pageno)
-        device.setcoords(pageannots)
-        interpreter.process_page(page)
-        allannots.extend(pageannots)
+            page.annots = getannots(pdfannots, page, codec)
+            page.annots.sort()
+            device.setcoords(page.annots)
+            interpreter.process_page(pdfpage)
+            allannots.extend(page.annots)
 
     if emit_progress:
         sys.stderr.write("\n")
@@ -353,7 +389,7 @@ def process_file(fh, emit_progress):
 
     device.close()
 
-    return (allannots, outlines, mediaboxes)
+    return (allannots, outlines)
 
 def parse_args():
     p = argparse.ArgumentParser(description=__doc__)
@@ -369,11 +405,13 @@ def parse_args():
                    help="wrap text at this many columns")
     p.add_argument("-p", "--progress", default=False, action="store_true",
                    help="emit progress information")
+    p.add_argument("-c", "--codec", default="cp1252", type=codecs.lookup,
+                   help="text encoding for annotations (default: windows-1252)")
     return p.parse_args()
 
 def main():
     args = parse_args()
-    annots = process_file(args.input, args.progress)
+    annots = process_file(args.input, args.codec, args.progress)
     prettyprint(annots, args.output, args.wrap, args.sections)
     return 0
 
