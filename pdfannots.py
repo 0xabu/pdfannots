ceadd1b388d7d9f23a81e469e35d40a51c1546bd
Andrew Baumann <Andrew.Baumann@microsoft.com>
2021-08-12 17:20:13 -0700

    reorder class declarations so we can avoid from __future__ import annotations

--- pdfannots.py
+++ pdfannots.py
@@ -5,8 +5,6 @@
 Extracts annotations from a PDF file in markdown format for use in reviewing.
 """
 
-from __future__ import annotations # XXX: Python 3.7 only; https://stackoverflow.com/questions/55320236/does-python-evaluate-type-hinting-of-a-forward-reference
-
 __version__ = '0.1'
 
 import sys, io, textwrap, argparse, datetime, logging
@@ -44,101 +42,15 @@ def cleanup_text(text:str) -> str:
     text = text.replace('\r\n', '\n').replace('\r', '\n')
     return ''.join([CHARACTER_SUBSTITUTIONS.get(c, c) for c in text])
 
+
 Box = typing.Tuple[float, float, float, float] # (x0, y0, x1, y1)
-
-class RectExtractor(TextConverter):
-    annots: typing.Set[Annotation]
-    _lasthit: typing.FrozenSet[Annotation]
-    _curline: typing.Set[Annotation]
-
-    def __init__(self, rsrcmgr, codec='utf-8', pageno=1, laparams=None):
-        dummy = io.StringIO()
-        TextConverter.__init__(self, rsrcmgr, outfp=dummy, codec=codec, pageno=pageno, laparams=laparams)
-        self.annots = set()
-
-    def setannots(self, annots: typing.Sequence[Annotation]) -> None:
-        self.annots = {a for a in annots if a.boxes}
-
-    # main callback from parent PDFConverter
-    def receive_layout(self, ltpage: LTPage) -> None:
-        self._lasthit = frozenset()
-        self._curline = set()
-        self.render(ltpage)
-
-    def testboxes(self, item: LTItem) -> typing.AbstractSet[Annotation]:
-        hits = frozenset({a for a in self.annots if any({self.boxhit(item, b) for b in a.boxes})})
-        self._lasthit = hits
-        self._curline.update(hits)
-        return hits
-
-    @staticmethod
-    def boxhit(item, box: Box) -> bool:
-        (x0, y0, x1, y1) = box
-        assert item.x0 <= item.x1 and item.y0 <= item.y1
-        assert x0 <= x1 and y0 <= y1
-
-        # does most of the item area overlap the box?
-        # http://math.stackexchange.com/questions/99565/simplest-way-to-calculate-the-intersect-area-of-two-rectangles
-        x_overlap = max(0, min(item.x1, x1) - max(item.x0, x0))
-        y_overlap = max(0, min(item.y1, y1) - max(item.y0, y0))
-        overlap_area = x_overlap * y_overlap
-        item_area = (item.x1 - item.x0) * (item.y1 - item.y0)
-        assert overlap_area <= item_area
-
-        if overlap_area != 0:
-            logging.debug("Box hit: '%s' %f-%f,%f-%f in %f-%f,%f-%f %2.0f%%",
-                item.get_text(), item.x0, item.x1, item.y0, item.y1, x0, x1, y0, y1,
-                100 * overlap_area / item_area)
-
-        if item_area == 0:
-            return False
-        else:
-            return overlap_area >= 0.5 * item_area
-
-    # "broadcast" newlines to _all_ annotations that received any text on the
-    # current line, in case they see more text on the next line, even if the
-    # most recent character was not covered.
-    def capture_newline(self) -> None:
-        for a in self._curline:
-            a.capture('\n')
-        self._curline = set()
-
-    def render(self, item: LTItem) -> None:
-        # If it's a container, recurse on nested items.
-        if isinstance(item, LTContainer):
-            for child in item:
-                self.render(child)
-
-            # Text boxes are a subclass of container, and somehow encode newlines
-            # (this weird logic is derived from pdfminer.converter.TextConverter)
-            if isinstance(item, LTTextBox):
-                self.testboxes(item)
-                self.capture_newline()
-
-        # Each character is represented by one LTChar, and we must handle
-        # individual characters (not higher-level objects like LTTextLine)
-        # so that we can capture only those covered by the annotation boxes.
-        elif isinstance(item, LTChar):
-            for a in self.testboxes(item):
-                a.capture(item.get_text())
-
-        # Annotations capture whitespace not explicitly encoded in
-        # the text. They don't have an (X,Y) position, so we need some
-        # heuristics to match them to the nearby annotations.
-        elif isinstance(item, LTAnno):
-            text = item.get_text()
-            if text == '\n':
-                self.capture_newline()
-            else:
-                for a in self._lasthit:
-                    a.capture(text)
 
 
 class Page:
     pageno: int
     mediabox: Box
     ncolumns: int
-    annots: typing.List[Annotation]
+    annots: typing.List["Annotation"]
 
     def __init__(self, pageno: int, mediabox: Box, ncolumns: int):
         self.pageno = pageno
@@ -151,6 +63,42 @@ class Page:
 
     def __lt__(self, other):
         return self.pageno < other.pageno
+
+
+class Pos:
+    def __init__(self, page: Page, x: float, y: float):
+        self.page = page
+        self.x = x
+        self.y = y
+
+    def __lt__(self, other):
+        if self.page < other.page:
+            return True
+        elif self.page == other.page:
+            assert self.page is other.page
+            # XXX: assume left-to-right top-to-bottom documents
+            (sx, sy) = self.normalise_to_mediabox()
+            (ox, oy) = other.normalise_to_mediabox()
+            (x0, y0, x1, y1) = self.page.mediabox
+            colwidth = (x1 - x0) / self.page.ncolumns
+            self_col = (sx - x0) // colwidth
+            other_col = (ox - x0) // colwidth
+            return self_col < other_col or (self_col == other_col and sy > oy)
+        else:
+            return False
+
+    def normalise_to_mediabox(self) -> typing.Tuple[float, float]:
+        x, y = self.x, self.y
+        (x0, y0, x1, y1) = self.page.mediabox
+        if x < x0:
+            x = x0
+        elif x > x1:
+            x = x1
+        if y < y0:
+            y = y0
+        elif y > y1:
+            y = y1
+        return (x, y)
 
 
 class Annotation:
@@ -227,41 +175,6 @@ class Annotation:
     def __lt__(self, other):
         return self.getstartpos() < other.getstartpos()
 
-
-class Pos:
-    def __init__(self, page: Page, x: float, y: float):
-        self.page = page
-        self.x = x
-        self.y = y
-
-    def __lt__(self, other):
-        if self.page < other.page:
-            return True
-        elif self.page == other.page:
-            assert self.page is other.page
-            # XXX: assume left-to-right top-to-bottom documents
-            (sx, sy) = self.normalise_to_mediabox()
-            (ox, oy) = other.normalise_to_mediabox()
-            (x0, y0, x1, y1) = self.page.mediabox
-            colwidth = (x1 - x0) / self.page.ncolumns
-            self_col = (sx - x0) // colwidth
-            other_col = (ox - x0) // colwidth
-            return self_col < other_col or (self_col == other_col and sy > oy)
-        else:
-            return False
-
-    def normalise_to_mediabox(self) -> typing.Tuple[float, float]:
-        x, y = self.x, self.y
-        (x0, y0, x1, y1) = self.page.mediabox
-        if x < x0:
-            x = x0
-        elif x > x1:
-            x = x1
-        if y < y0:
-            y = y0
-        elif y > y1:
-            y = y1
-        return (x, y)
 
 class Outline:
     def __init__(self, title: str, dest: typing.Any, pos: Pos):
@@ -548,6 +461,94 @@ def get_outlines(doc: PDFDocument, pageslist: typing.List[Page], pagesdict: typi
                 pos = Pos(page, targetx, targety)
                 result.append(Outline(title, destname, pos))
     return result
+
+
+class RectExtractor(TextConverter):
+    annots: typing.Set[Annotation]
+    _lasthit: typing.FrozenSet[Annotation]
+    _curline: typing.Set[Annotation]
+
+    def __init__(self, rsrcmgr, codec='utf-8', pageno=1, laparams=None):
+        dummy = io.StringIO()
+        TextConverter.__init__(self, rsrcmgr, outfp=dummy, codec=codec, pageno=pageno, laparams=laparams)
+        self.annots = set()
+
+    def setannots(self, annots: typing.Sequence[Annotation]) -> None:
+        self.annots = {a for a in annots if a.boxes}
+
+    # main callback from parent PDFConverter
+    def receive_layout(self, ltpage: LTPage) -> None:
+        self._lasthit = frozenset()
+        self._curline = set()
+        self.render(ltpage)
+
+    def testboxes(self, item: LTItem) -> typing.AbstractSet[Annotation]:
+        hits = frozenset({a for a in self.annots if any({self.boxhit(item, b) for b in a.boxes})})
+        self._lasthit = hits
+        self._curline.update(hits)
+        return hits
+
+    @staticmethod
+    def boxhit(item, box: Box) -> bool:
+        (x0, y0, x1, y1) = box
+        assert item.x0 <= item.x1 and item.y0 <= item.y1
+        assert x0 <= x1 and y0 <= y1
+
+        # does most of the item area overlap the box?
+        # http://math.stackexchange.com/questions/99565/simplest-way-to-calculate-the-intersect-area-of-two-rectangles
+        x_overlap = max(0, min(item.x1, x1) - max(item.x0, x0))
+        y_overlap = max(0, min(item.y1, y1) - max(item.y0, y0))
+        overlap_area = x_overlap * y_overlap
+        item_area = (item.x1 - item.x0) * (item.y1 - item.y0)
+        assert overlap_area <= item_area
+
+        if overlap_area != 0:
+            logging.debug("Box hit: '%s' %f-%f,%f-%f in %f-%f,%f-%f %2.0f%%",
+                item.get_text(), item.x0, item.x1, item.y0, item.y1, x0, x1, y0, y1,
+                100 * overlap_area / item_area)
+
+        if item_area == 0:
+            return False
+        else:
+            return overlap_area >= 0.5 * item_area
+
+    # "broadcast" newlines to _all_ annotations that received any text on the
+    # current line, in case they see more text on the next line, even if the
+    # most recent character was not covered.
+    def capture_newline(self) -> None:
+        for a in self._curline:
+            a.capture('\n')
+        self._curline = set()
+
+    def render(self, item: LTItem) -> None:
+        # If it's a container, recurse on nested items.
+        if isinstance(item, LTContainer):
+            for child in item:
+                self.render(child)
+
+            # Text boxes are a subclass of container, and somehow encode newlines
+            # (this weird logic is derived from pdfminer.converter.TextConverter)
+            if isinstance(item, LTTextBox):
+                self.testboxes(item)
+                self.capture_newline()
+
+        # Each character is represented by one LTChar, and we must handle
+        # individual characters (not higher-level objects like LTTextLine)
+        # so that we can capture only those covered by the annotation boxes.
+        elif isinstance(item, LTChar):
+            for a in self.testboxes(item):
+                a.capture(item.get_text())
+
+        # Annotations capture whitespace not explicitly encoded in
+        # the text. They don't have an (X,Y) position, so we need some
+        # heuristics to match them to the nearby annotations.
+        elif isinstance(item, LTAnno):
+            text = item.get_text()
+            if text == '\n':
+                self.capture_newline()
+            else:
+                for a in self._lasthit:
+                    a.capture(text)
 
 
 def process_file(fh: typing.BinaryIO, columns_per_page: int, emit_progress=False
