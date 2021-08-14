358cc0bf3d6aa5491e34e578662399dcf201da2f
Andrew Baumann <Andrew.Baumann@microsoft.com>
2021-08-13 17:19:10 -0700

    add tests for markdown printers
    (just testing that they don't crash, but this would have caught my last refactor bug)

--- tests.py
+++ tests.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 # mypy: ignore-errors
 
+import argparse
 from datetime import datetime, timedelta, timezone
 import unittest
 import pathlib
 import pdfannots
 import pdfannots.utils
+from pdfannots.printer.markdown import MarkdownPrinter, GroupedMarkdownPrinter
+
+
+class UnitTests(unittest.TestCase):
+    def test_decode_datetime(self):
+        datas = [
+            ("D:123456", None),  # defensive on bad datetimes
+            ("D:20190119212926-08'00'",
+             datetime(2019, 1, 19, 21, 29, 26, tzinfo=timezone(-timedelta(hours=8)))),
+            ("20200102030405Z0000",
+             datetime(2020, 1, 2, 3, 4, 5, tzinfo=timezone.utc)),
+            ("D:20101112191817", datetime(2010, 11, 12, 19, 18, 17)),
+        ]
+        for dts, expected in datas:
+            dt = pdfannots.utils.decode_datetime(dts)
+            self.assertEqual(dt, expected)
 
 
 class ExtractionTestBase(unittest.TestCase):
@@ -61,21 +78,6 @@ class ExtractionTests(ExtractionTestBase):
             self.assertEqual(o.title, expected)
 
 
-class UnitTests(unittest.TestCase):
-    def test_decode_datetime(self):
-        datas = [
-            ("D:123456", None),  # defensive on bad datetimes
-            ("D:20190119212926-08'00'",
-             datetime(2019, 1, 19, 21, 29, 26, tzinfo=timezone(-timedelta(hours=8)))),
-            ("20200102030405Z0000",
-             datetime(2020, 1, 2, 3, 4, 5, tzinfo=timezone.utc)),
-            ("D:20101112191817", datetime(2010, 11, 12, 19, 18, 17)),
-        ]
-        for dts, expected in datas:
-            dt = pdfannots.utils.decode_datetime(dts)
-            self.assertEqual(dt, expected)
-
-
 class Issue9(ExtractionTestBase):
     filename = 'issue9.pdf'
 
@@ -111,6 +113,57 @@ class Pr24(ExtractionTestBase):
         self.assertEqual(len(self.annots), len(EXPECTED))
         for a, expected in zip(self.annots, EXPECTED):
             self.assertEqual((a.tagname, a.contents, a.gettext()), expected)
+
+
+class PrinterTestBase(unittest.TestCase):
+    filename = 'hotos17.pdf'
+    columns_per_page = 2
+
+    def setUp(self):
+        path = pathlib.Path(__file__).parent / 'tests' / self.filename
+        with path.open('rb') as f:
+            (annots, outlines) = pdfannots.process_file(f, self.columns_per_page)
+            self.annots = annots
+            self.outlines = outlines
+
+
+class MarkdownPrinterTest(PrinterTestBase):
+    # There's not a whole lot of value in testing the precise output format,
+    # but let's make sure we produce a non-trivial result and don't crash.
+    def test_flat(self):
+        args = argparse.Namespace()
+        args.printfilename = True
+        args.wrap = None
+        args.condense = True
+
+        p = MarkdownPrinter(args)
+
+        linecount = 0
+        charcount = 0
+        for line in p('dummyfile', self.annots, self.outlines):
+            linecount += line.count('\n')
+            charcount += len(line)
+
+        self.assertGreater(linecount, 5)
+        self.assertGreater(charcount, 500)
+
+    def test_grouped(self):
+        args = argparse.Namespace()
+        args.printfilename = False
+        args.wrap = 80
+        args.condense = True
+        args.sections = GroupedMarkdownPrinter.ALL_SECTIONS
+
+        p = GroupedMarkdownPrinter(args)
+
+        linecount = 0
+        charcount = 0
+        for line in p('dummyfile', self.annots, self.outlines):
+            linecount += line.count('\n')
+            charcount += len(line)
+
+        self.assertGreater(linecount, 10)
+        self.assertGreater(charcount, 900)
 
 
 if __name__ == "__main__":
